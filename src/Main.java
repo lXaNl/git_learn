49821489c09f1c6a29d13a632d7c8a6c9b6dcff7
Ruslan <rsln.taghiyev@gmail.com>
2018-08-06 21:09:15 +0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    /**
+     * В начале для инициализации в гите
+     *      git config --global user.name <NAME>
+     *      git config --global user.email <EMAIL>
+     * Для инфы
+     *      git config --list
+     *  Для списка команд
+     *      git help
+     *  Для добавления в stage
+     *      git add <FILE_NAME>
+     *          Пример:
+     *          git add src/Main.java
+     *  Для добавления всех файлов
+     *      git add .
+     *
+     *
+     */
+
+}
